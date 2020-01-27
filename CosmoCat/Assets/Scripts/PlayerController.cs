2de46939b9c720d3b631c1a98617782b2b23f95c
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-01-27 21:55:12 +0100

    GroundCheck working

--- CosmoCat/Assets/Scripts/PlayerController.cs
+++ CosmoCat/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private float movementInputDirection;
-
-    private Rigidbody2D rb;
-
-
 
     public float movementSpeed = 10;
     public float jumpForce = 16;
+    public float groundCheckRadius;
 
+    private Rigidbody2D rb;
+    private Animator anim;
 
     private bool IsFacingRight = true;
+    private bool isWalking; 
+    private bool isGround;
+    private bool canJump;
+
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,12 +35,21 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimations();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate() 
     {
         ApplyMovement();
+        CheckSurroundings();
     }
+
+    private void CheckSurroundings()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
 
     private void CheckMovementDirection()
     {
@@ -45,7 +61,34 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        if(rb.velocity.x != 0)
+        {
+            isWalking = true;
+        }
+        else 
+        {
+            isWalking = false;
+        }
     }
+
+    private void UpdateAnimations()
+    {
+        anim.SetBool("isWalking", isWalking);
+    }
+
+
+    private void CheckIfCanJump() {
+        if(isGround && rb.velocity.y <= 0)
+        {
+            canJump = true;
+        } 
+        else 
+        {
+            canJump = false;
+        }
+    }
+
 
     private void CheckInput() 
     {
@@ -59,7 +102,10 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
+        if(canJump)
+        {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 
     private void ApplyMovement()
@@ -73,6 +119,9 @@ public class PlayerController : MonoBehaviour
        transform.Rotate(0.0f, 180.0f, 0.0f);
    }
 
-  
+  private void  OnDrawGizmos() 
+  {
+      Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+  }
 
 }
